9120ffd022d1244f5b58dad3e91f7971ac96749d
DESKTOP-484K4TT\diftandra <dift.febrianto@gmail.com>
2020-09-30 20:48:04 +0700

    Tugas-3

--- Tugas-3/tugas3.js
+++ Tugas-3/tugas3.js
@@ -32,9 +32,9 @@ var kalimat = 'wah javascript itu keren sekali';
 
 var kataPertama = kalimat.substring(0, 3); 
 var kataKedua = kalimat.substring(4,14); // do your own! 
-var kataKetiga = kalimat.substring(15,19); // do your own! 
-var kataKeempat = kalimat.substring(19,25); // do your own! 
-var kataKelima = kalimat.substring(25 ); // do your own! 
+var kataKetiga = kalimat.substring(15,18); // do your own! 
+var kataKeempat = kalimat.substring(19,24); // do your own! 
+var kataKelima = kalimat.substring(25); // do your own! 
 
 console.log('Kata Pertama: ' + kataPertama); 
 console.log('Kata Kedua: ' + kataKedua); 
